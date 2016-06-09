b9b712019ce9f65b08a0c8f1054bbc8e84596b62
Justin Force <justin.force@gmail.com>
2016-06-08 19:58:20 -0700

    Do chapter 3 exercise 1

--- chapter3/exercise1/src/exercise1/core.clj
+++ chapter3/exercise1/src/exercise1/core.clj
@@ -1,7 +1,25 @@
 (ns exercise1.core
   (:gen-class))
 
+(defn describe
+  "Describe the thing that was passed in"
+  [thing demo]
+  (println (str (name thing) ": " demo)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Use the str, vector, list, hash-map, and hash-set functions"
   [& args]
-  (println "Hello, World!"))
+
+  (describe 'str (str "this is" " a string"))
+
+  (let [my-vector [:foo :bar]]
+    (describe 'vector (first my-vector)))
+
+  (let [my-list '(:foo :bar)]
+    (describe 'list (rand-nth my-list)))
+
+  (let [my-hash-map (hash-map :foo "FOO" :bar "BAR")]
+    (describe 'hash-map (:foo my-hash-map)))
+
+  (let [my-hash-set #{:foo :bar}]
+    (describe 'hash-set (contains? my-hash-set :foo))))
